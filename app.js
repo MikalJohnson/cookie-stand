e0093e682b72cf0b9b5855e95420d6a02e2d8f31
Mikal Johnson <Johnsonmikal24@gmail.com>
2023-02-01 09:41:26 -0800

    Added Some of the JS

--- app.js
+++ app.js
@@ -1,58 +1,34 @@
 'use strict';
 
-const seattleStore = {
-  city: 'Seattle',
-  minCust: 23,
-  maxCust: 65,
-  avgCust: 6.3,
+// Set our hours array
+// 14 hours from 6 a.m to 8 p.m
+let hours = ['6 a.m', '7 a.m', '8 a.m', '9 a.m', '10 a.m', '11 a.m', '12 p.m', '1 p.m', '2 p.m', '3 p.m', '4 p.m', '5 p.m', '6 p.m', '7 p.m']
 
-  randomCustomers: function() {
-    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);  
+
+// CREATE STORE OBJECT
+// create separate JS object literals for each shop location.
+// Each object will generating sales data and output on sales html.
+let seattle = {
+  name: 'seattle',
+  min: 23,
+  max: 65,
+  avg: 6.3,
+  salesArray: [],
+  
+  // Use a method of that object to generate a random number of customers per hour. Objects/Math/random
+  randomNumOfCustomers: function() {
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  },
+
+  // Use a method of that object to generate a random number of customers per hour. Objects/Math/random
+  cookiesPerHour: function() {
+    for (let i = 0; i < hours.length; i++) {
+      let customers = this.randomNumOfCustomers();
+      let cookiesSoldThisHour = Math.ceil(customers * this.avg);
+      this.salesArray.push(cookiesSoldThisHour)
+      console.log(this.salesArray);
+    }
   }
 }
 
-console.log(seattleStore.randomCustomers(seattleStore.maxCust));
-
-const tokyoStore = {
-  city: 'Tokyo',
-  minCust: 3,
-  maxCust: 24,
-  avgCust: 1.2,
-
-  randomCustomers: function() {
-    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);  
-  }
-}
-
-const dubaiStore = {
-  city: 'Dubai',
-  minCust: 11,
-  maxCust: 38,
-  avgCust: 3.7,
-
-  randomCustomers: function() {
-    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);  
-  }
-}
-
-const parisStore = {
-  city: 'Paris',
-  minCust: 20,
-  maxCust: 38,
-  avgCust: 2.3,
-
-  randomCustomers: function() {
-    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);  
-  }
-}
-
-const limaStore = {
-  city: 'Lima',
-  minCust: 2,
-  maxCust: 16,
-  avgCust: 4.6,
-
-  randomCustomers: function() {
-    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);  
-  }
-}
+console.log(seattle.cookiesPerHour())
